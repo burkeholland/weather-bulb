97e4437cec441c9b601560b3ae27cf4f208cf92a
Burke Holland <burkeholland@Burkes-MacBook-Pro.local>
2018-03-16 13:42:11 -0500

    Comments are nice 📝

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # weather-bulb
+
 A Serverless timer functions which sets the color of an LIFX bulb based on the outside temperature
+
+## Setup Instructions
+
+Deploy it straight to Azure without cloning or installing anything. Easily your best option.
+[![Deploy to Azure](https://azuredeploy.net/deploybutton.png)](https://azuredeploy.net/)
+
+## Run Locally
+
+Requires the [Azure Functions Core Tools](https://docs.microsoft.com/en-us/azure/azure-functions/functions-run-local?wt.mc_id=weatherbulb-github-buhollan).
+
+Clone the repo. Add in a `local.settings.json` file. In that file you will need to specify the following format/keys...
+
+```
+{
+  "IsEncrypted": false,
+  "Values": {
+    "AzureWebJobsStorage": "YOUR-STORAGE-KEY",
+    "LIFX_TOKEN": "YOUR-LIFX-TOKEN",
+    "DS_API": "https://api.darksky.net/forecast",
+    "DS_SECRET": "YOUR-DARKSKY-SECRET",
+    "LAT": "YOUR-LATITUDE",
+    "LNG": "YOUR-LONGITUDE"
+  }
+}
+```
+
--- getWeather/index.js
+++ getWeather/index.js
@@ -1,3 +1,4 @@
+
 const axios = require('axios');
 const LIFX = require('lifx-http-api');
 
@@ -5,28 +6,33 @@ let client = new LIFX({
   bearerToken: process.env.LIFX_TOKEN
 });
 
-
 module.exports = function (context, myTimer) {
-
-  if (myTimer.isPastDue) {
-    context.log('JavaScript is running late!');
-  }
-
+  // build up the DarkSky endpoint
   let endpoint = `${process.env.DS_API}/${process.env.DS_SECRET}/${
     process.env.LAT
     },${process.env.LNG}`;
 
+  // use axios to call DarkSky for weather
   axios
     .get(endpoint)
     .then(data => {
       let temp = Math.round(data.data.currently.temperature);
-      let hue = 200 + (160 / (100 / temp));
+
+      // make sure the temp isn't above 100 because that's as high as we can go
+      temp = temp < 100 ? temp : 100;
+
+      // determine the hue
+      let hue = 200 + (160 * (temp / 100));
+
+      // return Promise.all so we can resolve at the top level
       return Promise.all([
         data,
         client.setState('all', { color: `hue:${hue}` })
       ]);
     })
     .then(result => {
+      // result[0] contains the darksky result
+      // result[1] contains the LIFX result
       context.log(result[1]);
     })
     .catch(err => {
